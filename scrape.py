3a70d3ff3ec66f4abf77d6c5fd34007848203286
Alberto Morón Hernández <alberto.moronh@gmail.com>
2017-03-24 23:00:14 +0000

    Define both halves of url.

--- scrape.py
+++ scrape.py
@@ -1,2 +1,6 @@
 from lxml import html
 import requests
+
+
+url1 = "http://chroniclingamerica.loc.gov/search/pages/results/?date1="
+url2 = "&searchType=advanced&language=&sequence=0&index=0&proxdistance=5&state=&rows=20&ortext=&proxtext=&phrasetext=iron+heel&andtext=&dateFilterType=yearRange&page=1"
