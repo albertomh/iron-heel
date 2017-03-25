4cb7e163c705b04c37f033537e095800e59e294a
Alberto Morón Hernández <alberto.moronh@gmail.com>
2017-03-25 11:19:02 +0000

    news() returns a list of newspapers found by a query.

--- scrape.py
+++ scrape.py
@@ -4,3 +4,12 @@ import requests
 
 url1 = "http://chroniclingamerica.loc.gov/search/pages/results/?date1="
 url2 = "&searchType=advanced&language=&sequence=0&index=0&proxdistance=5&state=&rows=20&ortext=&proxtext=&phrasetext=iron+heel&andtext=&dateFilterType=yearRange&page=1"
+
+def news(y1, y2):
+    """
+    Returns a list of the names of newspapers found for the query "iron heel".
+
+    """
+
+    html = requests.get(url1 + str(y1) + "&sort=date&date2=" + str(y2) + url2)
+    return re.findall('\=1\">(\w.*?\.)', str(html.content))
