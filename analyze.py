55420bf57e13f9ec3d78eaf7c73270a3084d455f
Alberto Morón Hernández <alberto.moronh@gmail.com>
2017-03-27 09:20:51 +0100

    placefinder() counts mentions per city and returns set of state codes.

--- analyze.py
+++ analyze.py
@@ -1,4 +1,5 @@
 import json
+from collections import Counter
 
 
 def read():
@@ -26,3 +27,16 @@ def countcat():
         l_cat.append(read()['data'][str(i)]['cat'])
 
     return l_cat
+
+
+def placefinder():
+
+    data = read()['data']
+    l_city = []
+    l_state = []
+
+    for i in range(0, len(data)):
+        l_city.append(data[str(i)]['loc'][0])
+        l_state.append(data[str(i)]['loc'][1])
+
+    print(Counter(l_city), '\n' * 2, set(l_state))
