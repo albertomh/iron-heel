1bc1b0ae1c0c9c7a578ff7c189b98467f52eced3
Alberto Morón Hernández <alberto.moronh@gmail.com>
2017-03-27 09:11:08 +0100

    Documented read().

--- analyze.py
+++ analyze.py
@@ -2,6 +2,11 @@ import json
 
 
 def read():
+    """
+    Read, parse and return 'tih-data.json', which contains 46 records
+    for mentions of Jack London's The Iron Heel in American newspapers.
+
+    """
 
     with open('tih-data.json') as infile:
         parsed_json = json.load(infile)
